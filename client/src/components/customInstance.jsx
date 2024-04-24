a98fb65e461d1eae0b3f57c51637ee73d6c83780
sawmeraw <pandeysameer96@gmail.com>
2024-04-24 22:54:36 +0930

    customInstance updated

--- client/src/components/customInstance.jsx
+++ client/src/components/customInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchData = axios.create({
-  baseURL: "https://image-fetcher-express.vercel.app/",
+  baseURL: "https://image-fetcher-express.vercel.app",
 });
 
 export default fetchData;
