a2343540a95d4f97edca982ec98f63e7c63665cb
sawmeraw <pandeysameer96@gmail.com>
2024-04-24 22:24:40 +0930

    cors updated

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use(cors({ origin: true, credentials: true }));
+
 const getUrlArray = (brand, productCode, colorCode) => {
   if (brand == "asics") {
     const urlArray = [
